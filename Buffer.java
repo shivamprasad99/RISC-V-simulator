d9555c0080f85092fea71ec1359f7acc7e5c60ac
hnysidhu <37617683+hnysidhu@users.noreply.github.com>
2019-04-17 17:52:42 +0530

    new buffer

---
+++ Buffer.java
@@ -0,0 +1,24 @@
+class Buffer{
+        String IR;
+        int ra;
+        int rb;
+        int rs1;
+        int rs2;
+        int rd;
+        int ry;
+        int rz;
+        int rm;
+        int pc_temp;
+        int memoryData;
+        int branch_next_pc;
+        int branch_prediction;
+        int immediate;
+        int which_instruction;
+        int forwarding1; // 1 for rz to ra, 2 for rz to rb, 3 for ry to ra , 4 for ry to rb,  5 for ry to rz , 6 ry to rm
+        int forwarding2;
+        Buffer(){
+                forwarding1=0;
+                forwarding2=0;
+                rd=-2;
+        }
+}
